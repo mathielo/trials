672502386f9359b86bf89a9f12f8c027679dd6a7
Caio Mathielo <mathielo@gmail.com>
2015-07-01 16:46:18 -0300

    Starting omniauth implementation with Google OAuht2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :omniauthable, :rememberable, :trackable
+  devise :rememberable, :trackable, :omniauthable, omniauth_providers: [:google_oauth2]
 end
