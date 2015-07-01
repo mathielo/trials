ac7ebfb00c7892b75a89e7572231f191e2689781
Caio Mathielo <mathielo@gmail.com>
2015-07-01 16:18:52 -0300

    Adding columns to User: provider, uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701175641) do
+ActiveRecord::Schema.define(version: 20150701191730) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150701175641) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
