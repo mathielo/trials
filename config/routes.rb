0225b7885452d51650684959e835a5d5a97c0cec
Caio Mathielo <mathielo@gmail.com>
2015-07-09 10:51:29 -0300

    Adding Zombie scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :zombies
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
